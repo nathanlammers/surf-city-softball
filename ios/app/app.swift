1a7ebac02863e30afa63423937dfa9b447d687fe
nathanlammers <nathanlammers11@gmail.com>
2025-02-15 14:51:05 -0800

    add in the ios app

---
+++ ios/app/app.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Nathan Lammers on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/tests/tests.swift
@@ -0,0 +1,17 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by Nathan Lammers on 2/15/25.
+//
+
+import Testing
+@testable import ios
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
