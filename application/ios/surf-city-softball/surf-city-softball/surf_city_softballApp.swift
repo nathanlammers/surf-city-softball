3b047be90f72df93dd803b2eee1c45294e76da1f
nathanlammers <nathanlammers11@gmail.com>
2025-02-15 08:47:46 -0800

    set up xcode project

---
+++ application/ios/surf-city-softball/surf-city-softball/surf_city_softballApp.swift
@@ -0,0 +1,17 @@
+//
+//  surf_city_softballApp.swift
+//  surf-city-softball
+//
+//  Created by Nathan Lammers on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct surf_city_softballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
